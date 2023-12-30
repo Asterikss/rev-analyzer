b3dfd68cfc86706d264a03243b8f5049b9eed65c
Andre <AndreAndreM@proton.me>
2023-12-30 14:11:47 +0100

    change layout

--- app.py
+++ app.py
@@ -19,21 +19,33 @@ def get_classifier():
 
 classifier = get_classifier()
 
-
-user_input = st.text_area("Enter a review to be analyzed")
+st.subheader("Enter a review to by analyzed below")
+st.write("Or choose a predefined review:")
 
 predefined_options = [
     "Red Hot Chili Peppers on vinyl has been a disappointing experience.. I had to return both “By The Way” and “Stadium Arcadium” because there were skips on almost all of it.. Kind of made it seem like the record label just went cheap, which is a disservice to anyone that actually listens to their vinyl...This “Greatest Hits” compilation did not have the same problems as the other two I bought. It sounded as it should have, and there were no skips.",
     "I love this shirt. It is really really soft"
 ]
 
-selected_option = st.selectbox("Or choose a predefined review:", predefined_options)
+button_0 = st.button(predefined_options[0])
+button_1 = st.button(predefined_options[1])
 
-analyze_button = st.button("Analyze")
+user_input = st.text_input(
+        "User input",
+        placeholder="Enter a review to be analyzed",
+        label_visibility="collapsed",
+        # key="user_query",
+    )
 
-input_to_analyze = selected_option if selected_option else user_input
+lbl_emoji_dict = {"music_album": "🎧",
+                  "apparel": "👢"}
 
-if input_to_analyze and analyze_button:
+
+if user_input or button_0 or button_1:
+    input_to_analyze = user_input if user_input else predefined_options[0] if button_0 else predefined_options[1]
     output = classifier([input_to_analyze])
     label = output[0]["label"] # type: ignore[reportOptionalSubscript]
-    st.write("Classified as: [  ", label, "  ] Confidence: ", output[0]["score"]) # type: ignore[reportOptionalSubscript]
+
+    emoji = lbl_emoji_dict[label] # type: ignore[GeneralTypeIssues]
+    with st.chat_message("user", avatar=emoji):
+        st.write("Classified as: [  ", label, "  ] Confidence: ", output[0]["score"]) # type: ignore[reportOptionalSubscript]
