0b8b056373681865c26be087e085599e21da67b7
Andre <AndreAndreM@proton.me>
2023-12-31 04:06:42 +0100

    This fix took me 3 hours. Fixed input deletion and output disappearing

--- 1_🔬_Analyzer.py
+++ 1_🔬_Analyzer.py
@@ -9,16 +9,6 @@ st.set_page_config(
     layout="wide"
 )
 
-page =  """
-<style>
-body {
-    background-color: #1E1E1E;
-    color: #FFFFFF;
-}
-</style>
-"""
-
-st.markdown(page, unsafe_allow_html=True)
 st.title("ReviewAnalyzer")
 
 @st.cache_resource
@@ -32,17 +22,22 @@ st.write("Or choose a predefined review:")
 
 predefined_options = [
     "Red Hot Chili Peppers on vinyl has been a disappointing experience.. I had to return both “By The Way” and “Stadium Arcadium” because there were skips on almost all of it.. Kind of made it seem like the record label just went cheap, which is a disservice to anyone that actually listens to their vinyl...This “Greatest Hits” compilation did not have the same problems as the other two I bought. It sounded as it should have, and there were no skips.",
-    "I love this shirt. It is really really soft"
+    "I've read a number of Stephen King's works over the past 15 years. King has always genuinely impressed me with his incredible eye for detail, his sense of place, and his ability to steadily pay out the rope line of a story's plot. Additionally, of course, he's the Jedi Master of creepiness. Although I was familiar with the premise of “The Stand”, it still scared me a lot",
 ]
 
-button_0 = st.button(predefined_options[0])
-button_1 = st.button(predefined_options[1])
+def clear_user_input():
+    st.session_state.user_input = ""
 
-user_input = st.text_input(
+
+button_0 = st.button(predefined_options[0], on_click=clear_user_input)
+button_1 = st.button(predefined_options[1], on_click=clear_user_input)
+
+
+st.text_input(
         "User input",
         placeholder="Enter a review to be analyzed",
         label_visibility="collapsed",
-        # key="user_query",
+        key="user_input",
     )
 
 lbl_emoji_dict = {"music_album": "🎧",
@@ -70,11 +65,16 @@ lbl_emoji_dict = {"music_album": "🎧",
                   "automotive": "🚗",
 }
 
+def update_state(input):
+    st.session_state.selected_text = input
+    
+if 'selected_text' not in st.session_state:
+    st.session_state.selected_text = ""
 
-if user_input or button_0 or button_1:
-    input_to_analyze = predefined_options[0] if button_0 else predefined_options[1] if button_1 else user_input
+if st.session_state.user_input or button_0 or button_1:
+    input_to_analyze = predefined_options[0] if button_0 else predefined_options[1] if button_1 else st.session_state.user_input
 
-    if len(input_to_analyze) < 20:
+    if len(input_to_analyze) < 25:
         st.warning("Remeber that the model needs to have some context. It might struggle if the input is to short")
 
     output = classifier([input_to_analyze])
@@ -89,11 +89,23 @@ if user_input or button_0 or button_1:
 
         if works_of_art:
             st.write("*", "Works of art found:")
-            for woa in works_of_art:
-                st.button(woa)
+            for text in works_of_art:
+                st.button(text, on_click=update_state, args=(text,))
 
         if people:
             st.write("*", "People mentioned:")
-            for woa in people:
-                st.button(woa)
+            for text in people:
+                st.button(text, on_click=update_state, args=(text,))
 
+        st.success("You can click any of the phrases to search the Wikipedia")
+
+
+
+if st.session_state.selected_text:
+    st.subheader("Phrase choosen: " + st.session_state.selected_text)
+    search_wiki = st.button("Search Wiki")
+
+    if search_wiki:
+        with st.spinner('4Wait for it...'):
+            st.write(st.session_state.selected_text)
+            st.success('Searching ...')
