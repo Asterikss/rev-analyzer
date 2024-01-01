5293e7e5dafdeac836e2c56ec930ed127eff2c0f
Andre <AndreAndreM@proton.me>
2024-01-01 19:17:12 +0100

    added custom softmax function

--- core/models.py
+++ core/models.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
-import torch.nn.functional as F  # TODO maby write softmax
 from typing import Tuple, List
+from math import exp
 
 from core.utils import get_id2label_dict
 
@@ -16,11 +16,20 @@ def get_classifier():
     )
 
 
+def custom_softmax(logits):
+    max_val = max(logits)
+    exps = [exp(logit - max_val) for logit in logits]
+    exps_sum = sum(exps)
+    return [exp / exps_sum for exp in exps]
+
+
 def query_classifier(input: str) -> Tuple[List[float], List[str]]:
     classifier_model, tokenizer = get_classifier()
 
     out = classifier_model(**(tokenizer(input, return_tensors="pt")))
-    probs = F.softmax(out.logits, dim=-1)[0].tolist()
+    # probs = F.softmax(out.logits, dim=-1)[0].tolist()
+    probs = custom_softmax(out.logits[0])
+
     sorted_probs, sorted_idxs = zip(
         *sorted(zip(probs, range(len(probs))), reverse=True)
     )
